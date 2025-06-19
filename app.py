59b42cc128dab28d23a8921faf6cbfd5cd9c252e
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2025-06-19 09:48:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Configuración del Router MikroTik (ahora con variables de entorno)
-MIKROTIK_HOST = 'https://e749-2806-370-976e-3572-ac80-781b-8eac-a874.ngrok-free.app'  # Dirección REST API del router
+MIKROTIK_HOST = 'https://941b-200-68-173-6.ngrok-free.app'  # Dirección REST API del router
 USERNAME = os.getenv('MIKROTIK_USER', 'admin')
 PASSWORD = os.getenv('MIKROTIK_PASSWORD', '1234567890')
 REQUEST_TIMEOUT = 10  # segundos
